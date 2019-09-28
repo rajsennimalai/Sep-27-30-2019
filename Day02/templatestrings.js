a791a8a355c534819ba90bac04d4ece01730ab07
prabhu-durasoft <iamprabhu@yahoo.com>
2019-09-28 15:02:35 +0530

    Day02

---
+++ Day02/templatestrings.js
@@ -0,0 +1,15 @@
+let name = "Sam";
+//let message = "Hello " + name;
+
+let message = `Hello ${name}`;
+console.log(message);
+
+//backtick operator
+
+let multiline = `
+Hello
+How are you?
+
+This is a para!
+`
+console.log(multiline);
